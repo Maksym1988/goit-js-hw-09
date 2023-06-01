a49fae698c8a3a3b51a533beb9aa41bfbce427e6
Maksym1988 <splien1988@gmail.com>
2023-06-01 16:44:20 +0100

    update task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', onChangeBodyColor);
+stopBtn.addEventListener('click', offChangeBodyColor);
+let timerId = null;
+
+function changeBodyColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function onChangeBodyColor() {
+  startBtn.disabled = true;
+  timerId = setInterval(changeBodyColor, 1000);
+}
+
+function offChangeBodyColor() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
